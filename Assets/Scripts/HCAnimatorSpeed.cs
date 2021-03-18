e49cd8b7d34ddea8d28921d4b456f3b9d175700b
farkrash <62850026+farkrash@users.noreply.github.com>
2021-03-18 15:06:20 +0200

    dance moves added

---
+++ Assets/Scripts/HCAnimatorSpeed.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HCAnimatorSpeed : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private float animSpeed = 1.5f;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        animator.speed = animSpeed;
+    }
+}
