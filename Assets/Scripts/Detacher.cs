c9acbf2b94b82a56905cd1d4789d0fbb05733770
farkrash <62850026+farkrash@users.noreply.github.com>
2021-03-22 15:21:00 +0200

    Ending for coffin scene

--- Assets/Scripts/Detacher.cs
+++ Assets/Scripts/Detacher.cs
@@ -6,6 +6,8 @@ public class Detacher : MonoBehaviour
 {
 
     private PlayerCoffinScript playerScript;
+    [SerializeField] private GameObject groundCover;
+    private float groundSpeed = 0.000001f;
     private void Awake()
     {
         playerScript = FindObjectOfType<PlayerCoffinScript>();
@@ -17,10 +19,21 @@ public class Detacher : MonoBehaviour
             other.transform.parent = transform.parent;
             playerScript.SpawnPointControll();
             other.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            MoveCover();
         }
         if (other.gameObject.CompareTag("Player"))
         {
             playerScript.stopMoving = true;
         }
+    }
+
+    private void MoveCover()
+    {
+        if(groundCover != null)
+        {
+            groundCover.transform.position = new Vector3(Mathf.Lerp(transform.position.x, groundCover.transform.position.x, Time.deltaTime * groundSpeed),
+                groundCover.transform.position.y, groundCover.transform.position.z);
+        }
+
     }
 }
