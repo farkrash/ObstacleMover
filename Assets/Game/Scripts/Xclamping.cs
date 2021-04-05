2f45acad4c62ccf1e1b3805033d8f2ca1ee14507
farkrash <62850026+farkrash@users.noreply.github.com>
2021-04-05 12:47:31 +0300

    fixes to xclamping script. now  works!

--- Assets/Game/Scripts/Xclamping.cs
+++ Assets/Game/Scripts/Xclamping.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class Xclamping : MonoBehaviour
 {
-    private Quaternion myRotatin;
-    [SerializeField] private float maxRot = 20f;
-
-    private void Start()
-    {
-        myRotatin = transform.localRotation;
-    }
-
     private void Update()
     {
-        myRotatin.x = Mathf.Clamp(myRotatin.x, -maxRot, maxRot);
-        print("MyRotatin" + myRotatin.x);
-        print("localRotation" + transform.localRotation.x);
+        //transform.eulerAngles = Vector3(0, ClampAngle(angle, ang0 - 40, ang0 + 40), 0);
+        transform.eulerAngles = new Vector3(ClampAngle(transform.eulerAngles.x, -20, 20) ,0 , 0 );
     }
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < 90 || angle > 270)
+        {       // if angle in the critic region...
+            if (angle > 180) angle -= 360;  // convert all angles to -180..+180
+
+            if (max > 180) max -= 360;
+            if (min > 180) min -= 360;
+        }
+        angle = Mathf.Clamp(angle, min, max);
+        if (angle < 0) angle += 360;  // if angle negative, convert to 0..360
+        return angle;
+    }
+
 }
